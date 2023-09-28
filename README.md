352e74ad38179da32f8a1d5d878ad3ec367a5c84
Arthwur <142269162+Arthwwr420@users.noreply.github.com>
2023-09-28 16:18:56 -0600

    avance de listas

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Generador de imagenes chistosas
 ## Contexto
 
-La comedia en los ultimos aÃ±os ha sido sumamente influenciada por imagenes chistosas en el internet, o memes, eventualmente llegando a la creacion de paginas dedicadas a estos.  Un usuario, conocido como JECR147, decidio hacer algo un tanto mas, distinto. Desarrollo un algoritmo capaz de combinar plantillas y source images, generando asi memes de manera completamente para subir a redes sociales. A este proyecto se le conoce como Momosbot4000, con cuentas en redes sociales como Twitter en las cuales publica estos memes de manera periodica.
+La comedia en los ultimos años ha sido sumamente influenciada por imagenes chistosas en el internet, o memes, eventualmente llegando a la creacion de paginas dedicadas a estos.  Un usuario, conocido como JECR147, decidio hacer algo un tanto más, distinto. Desarrolló un algoritmo capaz de combinar plantillas y source images, generando asi memes de manera completamente para subir a redes sociales. A este proyecto se le conoce como Momosbot4000, con cuentas en redes sociales como Twitter en las cuales publica estos memes de manera periodica.
 
-Sin embargo, debido a la completa aleatoriedad al momento de generar las imagenes, el resultado termina siendo muy inconsistente, habiendo imagenes que simplemente funcionan mejores que otras. A partir de esto, surge este proyecto, que busca crear un programa similar pero que pueda generar imagenes mï¿½s congruentes, pero sin perder por completo la aleatoriedad.
+Sin embargo, debido a la completa aleatoriedad al momento de generar las imagenes, el resultado termina siendo muy inconsistente, habiendo imagenes que simplemente funcionan mejores que otras. A partir de esto, surge este proyecto, que busca crear un programa similar pero que pueda generar imagenes más congruentes, pero sin perder por completo la aleatoriedad.
 
-Este programa tiene como objetivo editar imagenes, o plantillas, aÃ±adiendo texto u otras imagenes con fines comedicos, aunque sin estar limitado a estos, de manera aleatoria. Selecciona una imagen base o plantilla de manera aleatoria, la cual despues modificara aÃ±adiendo otras imagenes (las cuales varian en numero segun la plantilla elegida), las cuales tambien serï¿½n elegidas de manera procedural, siguiendo parametros dados por etiquetas para que la imagen divertida resultado del producto de estas tenga mas sentido. Estas etiquetas seran la base para el programa, pues permitiran al usuario decidir si se desea una imagen completamente aleatoria, sin sentido, o una mas convencional haciendo que las etiquetas de las imagenes combinadas coincidan.
+Este programa tiene como objetivo editar imagenes, o plantillas, añadiendo texto u otras imagenes con fines comedicos, aunque sin estar limitado a estos, de manera aleatoria. Selecciona una imagen base o plantilla de manera aleatoria, la cual después modificara añadiendo otras imagenes (las cuales varian en numero segun la plantilla elegida), las cuales tambien serán elegidas de manera procedural, siguiendo parametros dados por etiquetas para que la imagen divertida resultado del producto de estas tenga mas sentido. Estas etiquetas seran la base para el programa, pues permitiran al usuario decidir si se desea una imagen completamente aleatoria, sin sentido, o una mas convencional haciendo que las etiquetas de las imagenes combinadas coincidan.
 
 ### Intrucciones
 Descarga el archivo generador.py junto a las carpetas con las imagenes, esto debido a que el script buscara esas carpetas y esas imagenes mediante sus nombres asi que no los cambies. El script y las carpetas deben de estar en el mismo folder. 
@@ -13,12 +13,22 @@ El script usa Pillow, por lo que debe de ser instalado primero, afortunadamente,
 
 	python -m pip install --upgrade Pillow
 
-Si eso no funciona, puedes consultar la pagina web de la instalaciï¿½n: https://pillow.readthedocs.io/en/latest/installation.html
+Si Pillow ya esta instalado, deberia regresar un mensaje similar al siguiente:
+
+	Requirement already satisfied: Pillow in c:\users\Nombre\appdata\roaming\python\python311\site-packages (10.0.0)
+
+Si eso no funciona, puedes consultar la pagina web de la instalación: https://pillow.readthedocs.io/en/latest/installation.html
 Una vez instalado todo, correr con el siguiente comando:
 
 	python generador.py
 
-La imagen final sera guardada en el folder en el que se encuentren el script y las carpetas de imagenes, por lo que recomiendo se guarden en una carpeta aparte para que la imagen no se pierda entre los archivos. La imagen generada sera de nombre "newimage.png"
+Sigue las instrucciones dadas por el programa
+Las imagenes finales será guardada en el folder en el que se encuentren el script y las carpetas de imagenes, por lo que recomiendo que estos se guarden en una carpeta aparte para que la imagen no se pierda entre los archivos o  se instale directamente el ZIP. La imagen generada será de nombre "new image(numero de la imagen).png".
+
+#### Nota: Se generara el numero de imagenes que se desee, pero al volver a correr el archivo las imagenes generadas serán sobreescritas con las nuevas
+
+### Avance:
+En este avance se incorpora el uso de listas para plantillas con mas de un espacio para source image.
 
 ## Algoritmo
 El funcionamiento base del programa se basaria en lo siguiente
--- generador.py
+++ generador.py
@@ -1,6 +1,12 @@
 import random
 import os
-from PIL import Image
+try:
+    from PIL import Image
+except:
+    print('Pillow no encontrado, asegurate de haberlo instalado siguiendo las instrucciones del readme\n\
+Cerrando programa')
+    quit()
+
 """
 Generador de imagenes chistosas
 El programa final elige 2 de distintas carpetas para compararlas o juntarlas
@@ -8,8 +14,10 @@ Las imagenes son de 2 tipos: sources y plantillas o templates. La principal difr
 es que las sources estan pensadas para ser colocadas en las plantillas, por lo que las
 plantillas incluyen mas datos entre los que estan el numero de sources que admiten
 
-Esta version del programa permite al usuario elegir cuantas imagenes se generarán y de que
-manera se generarán, guardando la imagen final en el folder en el cual se encuentra este script
+Esta version del programa incorpora plantillas con varios espacios de source images, 
+por lo que ahora la clase de template toma tuples que a su vez contienen los parametros
+para cada espacio de la imagen
+Hasta ahora, la unica plantilla con mas de un espacio es TMP5
 """
 
 SOURCE_DIR = 'sources'
@@ -23,10 +31,9 @@ os.chdir(os.path.dirname(__file__))
 class source_image:
     #funcion al crear nuevo objeto,
     #toma el nombre de imagen y una etiqueta, abre la imagen a partir del nombre
-    def __init__(self, image: str, tag: str, tag2: str):
+    def __init__(self, image: str, tags: list):
         self.image = image
-        self.tag = tag
-        self.tag2 = tag2
+        self.tags = tags
         os.chdir(SOURCE_DIR)
         self.img = Image.open(image)
         os.chdir(os.path.dirname(__file__))
@@ -37,47 +44,63 @@ class source_image:
 #Plantilla: para guardar los datos, incluyendo etiquetas y numero de sources aceptadas
 class template:
     #funcion al crear nuevo objeto (plantilla),
-    #Toma el nombre de imagen. tamaño en pixeles del espacio para la source, coordenadas para dicho espacio,
-    #una etiqueta y el numero de sources que puede contener la imagen
-    def __init__(self, image: str, size: tuple, coor: tuple,  tag: str, source_capacity: int):
+    #Toma el nombre de imagen. tamaño en pixeles de los espacios para la source, coordenadas para dichos espacios,
+    #una etiqueta por espacio y el numero de sources que puede contener la imagen
+    def __init__(self, image: str, size: tuple, coor: tuple,  tag: tuple, source_capacity: int):
         self.image = image
-        self.size = size
-        self.tag = tag
-        self.coor = coor
+        every = [size, coor, tag]
+        for i in every:
+            if len(i) != source_capacity:
+                raise Exception("Amount of iterates in tuples must be equal to source capacity ", i, len(i))
         self.source_capacity = int(source_capacity)
+        self.slot = []
+        for i in range(self.source_capacity):
+            self.slot.append(place(size[i], coor[i], tag[i]))
         os.chdir(TEMP_DIR)
         self.img = Image.open(image)
         os.chdir(os.path.dirname(__file__))
         global n_of_temp
         n_of_temp +=1
 
+#Clase de cada espacio de la plantilla
+#Toma el tamaño del espacio, coordenadas y etiqueta
+class place:
+    def __init__(self, size: tuple, coor: tuple, tag: str):
+        self.size = size
+        self.coor = coor
+        self.tag = tag
+
 
 #intentar combinar un ejemplo de plantilla y un ejemplo de source, regresa la imagen final
-def combine(src:source_image, temp:template):
-    diference = temp.size
-    if(diference != 0):
-        new_size = temp.size
-        resize = src.img.resize(temp.size)
-        final =  temp.img
-        final.paste(resize, temp.coor)
-        print("Redimension necesaria. El tamaño de la source image ahora es", new_size)
+def combine(srcs: list, temp:template):
+    final =  temp.img
+    i = 0
+    for slotn in temp.slot:
+        diference = (slotn.size[0] != srcs[i].img.size[0] and slotn.size[1] != srcs[i].img.size[1])
+        if(diference):
+            new_size = slotn.size
+            resize = srcs[i].img.resize(slotn.size)
+            final.paste(resize, slotn.coor)
+            print("Redimension necesaria. El tamaño de la source image ahora es", new_size)
         
-    else:
-        print("No hubo necesidad de redimension")
-        final = temp.img
-        final.paste(src.img, temp.coor)
+        else:
+            print("No hubo necesidad de redimension")
+            final.paste(srcs[i].img, slotn.coor)
+        i +=1
 
     print("Imagenes combiadas:")
     print("Plantilla: ", temp.image)
-    print("Source: ", src.image)
+    print("Source: ")
+    for x in srcs:
+        print(x.image)
     return final
 
-#Comprueba 
-def are_tags_same(src:source_image, temp:template):
-    if src.tag == temp.tag or src.tag2 == temp.tag:
-        return True
-    else:
-        return False
+#Comprueba etiquetas
+def are_tags_same(src:source_image, slot:place):
+    for x in src.tags:
+        if x == slot.tag:
+            return True
+    return False
     
 #Toma 2 numeros y regresa la plantilla y SI referentes a dichos numeros
 def get_src_temp(src_num, tmp_num):
@@ -108,55 +131,79 @@ def get_src_temp(src_num, tmp_num):
             temp = TMP3
         case 4:
             temp = TMP4
+        case 5:
+            temp = TMP5
         case _:
             raise TypeError("Number given exceeds the number of Source Images or is less than 0")
     return s_img, temp
 
+#vuelve a intentar conseguir una SI con etiqueta igual a la del espacio, regresa hasta que consiga una
+def try_again(slot:place):
+    possible = False
+    while not possible:
+        print("Etiquetas no coinciden, volviendo a intentar")
+        RNG = random.randrange(0, n_of_src)
+        src_img, x = get_src_temp(RNG, 0)
+        del x
+        possible = are_tags_same(src_img, slot)
+    if possible:
+        return src_img
+        
 
 #Para imagenes aleatorias
 def random_image():
     RNG = random.randrange(0, n_of_src)
     RNG2 = random.randrange(0, n_of_temp)
     s_img, temp = get_src_temp(RNG, RNG2)
+    sources = [s_img]
+    if temp.source_capacity == 1:
+        return combine(sources, temp)
+    else:
+        for x in range(len(temp.slot)-1):
+            RNG = random.randrange(0, n_of_src)
+            s_img, t = get_src_temp(RNG, RNG2)
+            del t
+            sources.append(s_img)
+        return combine(sources, temp)
     
-    return combine(s_img, temp)
     
 #Para etiquetas
 def tags():
     RNG = random.randrange(0, n_of_src)
     RNG2 = random.randrange(0, n_of_temp)
     src_img, tmp = get_src_temp(RNG, RNG2)
-    possible = are_tags_same(src_img, tmp)
+    sources = [src_img]
+    possible = False
 
-    while not possible:
-        if(tmp.tag == src_img.tag) or (tmp.tag == src_img.tag2):
-            possible = True 
-            break 
-        else:
-            RNG = random.randrange(0, 2)
-            src_img, x = get_src_temp(RNG, 0)
-            print("Etiquetas no coinciden, volviendo a intentar")
+    if tmp.source_capacity == 1:
+        possible = are_tags_same(src_img, tmp.slot[0])
+        if not possible:
+            src_img = try_again(tmp.slot[0])
+            sources[0] = src_img
+            possible = True
+
+    else:
+        for x in range(len(tmp.slot)):
+            RNG = random.randrange(0, n_of_src)
+            possible = are_tags_same(s_img, tmp.slot[x])
+            if possible:
+                sources[x] = src_img
+            else:
+                src_img = try_again(tmp.slot[x])
+                sources[x] = src_img
+            
+            s_img, t = get_src_temp(RNG, RNG2)
+            del t
 
     if possible:
-        return combine(src_img, tmp)
+        return combine(sources, tmp)
     
 
 #Para eleccion manual de imagenes
 def manual():
     print("Has escogido la seleccion manual de imagenes \n Actualmente hay ", n_of_src, " source images y ", n_of_temp, " plantillas")
-    print("Cada imagen tiene un numero asignado, en sources estas van de 0 a ", (n_of_src -1), ". Ingresa un numero para\
- seleccionar la source")
-    select = int(input())
-
-    if select >= n_of_src:
-        print("Numero de Source Image no valido")
-        return
-    
-    src, x = get_src_temp(select, 0)
-    del x
-    print("Tu imagen tiene de nombre " + src.image)
-
-    print("Cada plantilla tambien tiene su numero, del 0 al ", (n_of_temp -1), ". Selecciona una mediante este numero")
+    print("Cada imagen tiene un numero asignado, en plantillas estas van de 0 a ", (n_of_temp -1), ". Ingresa un numero para\
+ seleccionar la plantilla")
     select = int(input())
 
     if select >= n_of_src:
@@ -167,24 +214,41 @@ def manual():
     del x
     print("Tu plantilla seleccionada tiene de nombre "+  tmp.image)
 
+    print ("La plantilla seleccionada tiene espacio para ", tmp.source_capacity, "sources, por lo que deberas seleccionar ", tmp.source_capacity)
+    sources = []
+    for x in range(tmp.source_capacity):
+        print("Cada source tambien tiene su numero, del 0 al ", (n_of_src -1), ". Selecciona una mediante este numero")
+        select = int(input())
+
+        if select >= n_of_src:
+            print("Numero de Source Image no valido")
+            return
+    
+        src, x = get_src_temp(select, 0)
+        del x
+        sources.append(src)
+        print("Tu imagen tiene de nombre " + src.image)
+
     select = int(input("Deseas continuar con la combinacion de estas imagenes? Si - 1, No - 0"))
     if select == 1:
-        return combine(src, tmp)
+        return combine(sources, tmp)
     else:
         return
 
 #definicion de cada imagen
-SRC0 = source_image("SOURCE(0).png", "anime", "desamparo")
-SRC1 = source_image("SOURCE(001).png", "animal", "ser")
-SRC2 = source_image("SOURCE(002).png", "ser", "sinsentido")
-SRC3 = source_image("SOURCE(003).png", "feliz", "ser")
-SRC4 = source_image("SOURCE(004).PNG", "agresivo", "ser")
-SRC5 = source_image("SOURCE(005).png", "Molesto", "sinsentido")
-TMP0 = template("TEMP(0).png", (300, 300), (315, 180), "ser", 1)
-TMP1 = template("TEMP(001).PNG", (1080, 526), (0, 134), "ser", 1)
-TMP2 = template("TEMP(002).png", (1080, 815), (0, 168), "sinsentido", 1)
-TMP3 = template("TEMP(003).png", (691, 495), (0, 99), "agresivo", 1)
-TMP4 = template("TEMP(004).png", (187, 125), (69, 152), "ser", 1)
+SRC0 = source_image("SOURCE(0).png", ["anime", "desamparo", "cara"])
+SRC1 = source_image("SOURCE(001).png", ["animal", "ser", "cara"])
+SRC2 = source_image("SOURCE(002).png", ["ser", "sinsentido"])
+SRC3 = source_image("SOURCE(003).png", ["feliz", "ser"])
+SRC4 = source_image("SOURCE(004).PNG", ["agresivo", "ser", "cara"])
+SRC5 = source_image("SOURCE(005).png", ["molesto", "sinsentido", "cara"])
+TMP0 = template("TEMP(0).png", ((300, 300),), ((315, 180),), ("ser",), 1)
+TMP1 = template("TEMP(001).PNG", ((1080, 526),), ((0, 134),), ("ser",), 1)
+TMP2 = template("TEMP(002).png", ((1080, 815),), ((0, 168),), ("sinsentido",), 1)
+TMP3 = template("TEMP(003).png", ((691, 495),), ((0, 99),), ("agresivo",), 1)
+TMP4 = template("TEMP(004).png", ((187, 125),), ((69, 152),), ("ser",), 1)
+TMP5 = template("TEMP(005).png", ((100, 115), (76, 92), (68, 80), (72, 85), (74, 91)), ((192, 0), (110, 25), (22, 42), (304, 43), (419, 54)), 
+("cara", "cara", "cara", "cara", "cara"), 5)
 
 #codigo principal
 print("Generador de imagenes V0.2")
@@ -199,16 +263,19 @@ while i < num_of_images:
         inp = int(input())
         if inp == 1:
             f = random_image()
-            f.save("new image.png")
+            name = str("new image("+ str(i) + ").png")
+            f.save(name)
             done = True
         elif inp == 2:
             f = tags()
-            f.save("new image.png")
+            name = str("new image("+ str(i) + ").png")
+            f.save(name)
             done = True
         elif inp == 3:
             f = manual()
             if f != None:
-                f.save("new image.png")
+                name = str("new image("+ str(i) + ").png")
+                f.save(name)
             done = True
         else:
             print("Tu respuesta no esta incluida en la lista de posibles respuestas, recuerda que solo acepta\
